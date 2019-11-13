d3b6e2e568f406888792ebc8b98e988957ac1b27
Valery Jacobs <valeryjacobs@hotmail.com>
2019-11-13 23:31:25 +0100

    Update nodedata.js

--- src/shared/nodedata.js
+++ src/shared/nodedata.js
@@ -7,7 +7,7 @@ const getNodeData = async function () {
 
   const blobServiceClient = new BlobServiceClient(
     // When using AnonymousCredential, following url should include a valid SAS or support public access
-    `https://jobblr.blob.core.windows.net/?sv=2019-02-02&ss=b&srt=sco&sp=rwdlac&se=2024-11-14T05:38:12Z&st=2019-11-13T21:38:12Z&spr=https&sig=Myqspe4jWnNT87Fndk6ntoViRIguYDby8cY7yTRlGZc%3D`,
+    ``,
     anonymousCredential
   );
 
